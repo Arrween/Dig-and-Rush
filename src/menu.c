ebc6243d7eeb2185b35d8d2325b1ce3c952edeb5
Ania Garoui <ania.garoui.etu@univ-lemans.fr>
2024-02-23 12:25:52 +0100

    Création d'un menu.c et menu.h pour séparer les fonctions d'initialisation

--- src/menu.c
+++ src/menu.c
@@ -1,8 +1,7 @@
-/*#include <stdio.h>
+#include <stdio.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-
-#include "tour.h"
+#include <SDL2/SDL_ttf.h>
 #include "menu.h"
 
 #define TITRE_FENETRE "Dig & Rush"
@@ -10,26 +9,26 @@
 #define TAILLE_H 480
 #define FPS 60
 
-int main() {
-    SDL_Window * fenetre;
-    SDL_Renderer * rend;
-    SDL_Event event;
-    SDL_Texture * textureFond ; // Texture pour l'image de fond
-    int fin = 0;
-    
+// Fonction qui initialise la SDL
+void initialiser_sdl(){
     if (SDL_Init(SDL_INIT_VIDEO)) {
         fprintf(stderr, "Erreur SDL_Init : %s\n", SDL_GetError());
         exit(EXIT_FAILURE);
     }
+}
 
-    // Initialisation de SDL_image pour le support des images PNG
+// Fonction qui initialise la SDL_IMG
+void initialiser_sdl_img(){
     if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
         fprintf(stderr, "Erreur IMG_Init : %s\n", IMG_GetError());
         SDL_Quit();
         exit(EXIT_FAILURE);
     }
+}
 
-    // Création de la fenêtre
+// Fonction qui crée la fenêtre du jeu
+SDL_Window * creation_fenetre(){
+    SDL_Window * fenetre ;
     if (!(fenetre = SDL_CreateWindow(TITRE_FENETRE,
 				    SDL_WINDOWPOS_CENTERED,
 				    SDL_WINDOWPOS_CENTERED,
@@ -40,59 +39,54 @@ int main() {
         SDL_Quit();
         exit(EXIT_FAILURE);
     }
+    return (fenetre);
+}
 
-    // Création du renderer
-    if (!(rend = SDL_CreateRenderer(fenetre, -1, 0))) {
+// Fonction qui crée le renderer
+SDL_Renderer * creation_renderer(SDL_Window ** fenetre){
+    SDL_Renderer * rend ;
+    if (!(rend = SDL_CreateRenderer(*fenetre, -1, 0))) {
         fprintf(stderr, "Erreur SDL_CreateRenderer : %s\n", SDL_GetError());
-        SDL_DestroyWindow(fenetre);
+        SDL_DestroyWindow(*fenetre);
         SDL_Quit();
         exit(EXIT_FAILURE);
     }
+    
+    return rend;
+}
 
-    // Chargement de l'image PNG dans une texture
-    if (! (textureFond = IMG_LoadTexture(rend,"../ressources/Menu/test.jpg")) ){
+//Fonction qui charge l'image de fond
+SDL_Texture * chargement_image(char * chemin, SDL_Renderer ** rend, SDL_Window ** fenetre){
+    SDL_Texture * texture_fond ;
+    if (!(texture_fond = IMG_LoadTexture(*rend, chemin)) ){
         fprintf(stderr, "Erreur lors du chargement de l'image : %s\n", IMG_GetError());
-        SDL_DestroyRenderer(rend);
-        SDL_DestroyWindow(fenetre);
+        SDL_DestroyRenderer(*rend);
+        SDL_DestroyWindow(*fenetre);
         IMG_Quit();
         SDL_Quit();
         exit(EXIT_FAILURE);
     }
-
-    while (!fin) {
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-
-		case SDL_QUIT:
-		    fin = 1;
-		    break;
-
-		case SDL_KEYDOWN:
-		    switch (event.key.keysym.scancode) {
-			case SDL_SCANCODE_ESCAPE:
-			case SDL_SCANCODE_Q:
-			    fin = 1;
-			    break;
-			default:
-			    break;
-		    }
-	    }
-	}
-
-       // Nettoyer le renderer
-	SDL_RenderClear(rend);
-	// Dessiner la texture de fond
-	SDL_RenderCopy(rend, textureFond, NULL, NULL);
-	// Mettre à jour l'écran
-	SDL_RenderPresent(rend);
-	SDL_Delay(1000/FPS);
-    }
-    // Nettoyage
-    SDL_DestroyTexture(textureFond);
-    SDL_DestroyRenderer(rend);
-    SDL_DestroyWindow(fenetre);
-    IMG_Quit();
-    SDL_Quit();
-    exit(EXIT_SUCCESS);
+    return texture_fond ;
 }
-*/
+
+// Fonction qui initialise la SDL_ttf
+void initialiser_sdl_ttf() {
+    if (TTF_Init() == -1) {
+        fprintf(stderr, "Erreur TTF_Init : %s\n", TTF_GetError());
+        SDL_Quit();
+        exit(EXIT_FAILURE);
+    }
+}
+
+// Fonction qui charge la police à partir d'un certain chemin
+TTF_Font * chargement_font(char * chemin, int taille, SDL_Renderer ** rend, SDL_Window ** fenetre) {
+    TTF_Font * font = TTF_OpenFont(chemin, taille);
+        if (font == NULL) {
+            fprintf(stderr, "Erreur TTF_OpenFont : %s\n", TTF_GetError());
+            SDL_DestroyWindow(*fenetre);
+            SDL_DestroyRenderer(*rend);
+            SDL_Quit();
+            exit(EXIT_FAILURE);
+        }
+    return font ;
+}
