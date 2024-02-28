f095c1043e4c0b44c36b26d04871dde3ea77f59b
Matthieu Boulanger <matthieu.boulanger.etu@univ-lemans.fr>
2024-02-28 02:36:28 +0100

    ajout d’une structure pour l’affichage des objets

--- src/tour.c
+++ src/tour.c
@@ -2,29 +2,20 @@
 
 #include "tour.h"
 #include "config.h"
-
-typedef struct {
-    SDL_Texture* texture;
-} t_objet;
-
-void init_jeu(SDL_Renderer * rend, t_objet *objets) {
-}
+#include "affichage.h"
 
 void boucle_jeu(SDL_Renderer * rend) {
     SDL_Event event;
     int fin = 0;
-    t_objet *objets;
 
-    SDL_Texture *fond, *fond_tour, *mur, *perso;
-    SDL_Rect rect_mur, rect_perso;
+    SDL_Texture * fond, * fond_tour, * mur, * perso;
+    t_affichage * aff_fond, * aff_fond_tour, * aff_mur_g, * aff_mur_d, * aff_perso;
+
     int largeur_tour = TAILLE_L/2;
     int x_tour = (TAILLE_L-largeur_tour)/2;
-    SDL_Rect rect_fond_tour = {x_tour, 0, largeur_tour, TAILLE_H};
     int largeur_mur = TAILLE_L/2/8;
     int largeur_perso = TAILLE_L/20;
     int hauteur_perso = TAILLE_H/20;
-
-    objets = malloc(sizeof(t_objet));
 
     fond = SDL_CreateTexture(rend, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, TAILLE_L, TAILLE_H);
     fond_tour = SDL_CreateTexture(rend, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, largeur_tour, TAILLE_H);
@@ -46,6 +37,12 @@ void boucle_jeu(SDL_Renderer * rend) {
 
     SDL_SetRenderTarget(rend, NULL);
 
+    aff_fond = creer_affichage(fond, -1, -1, -1, -1);
+    aff_fond_tour = creer_affichage(fond_tour, x_tour, 0, largeur_tour, TAILLE_H);
+    aff_mur_g = creer_affichage(mur, x_tour, 0, largeur_mur, TAILLE_H);
+    aff_mur_d = creer_affichage(mur, x_tour + largeur_tour - largeur_mur, 0, largeur_mur, TAILLE_H);
+    aff_perso = creer_affichage(perso, x_tour + 3*largeur_mur, TAILLE_H/5, largeur_perso, hauteur_perso);
+
     while (!fin) {
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
@@ -62,25 +59,19 @@ void boucle_jeu(SDL_Renderer * rend) {
 	    }
         SDL_RenderClear(rend);
 
-        SDL_RenderCopy(rend, fond, NULL, NULL);
-        SDL_RenderCopy(rend, fond_tour, NULL, &rect_fond_tour);
-        rect_mur.x = x_tour;
-        rect_mur.y = 0;
-        rect_mur.w = largeur_mur;
-        rect_mur.h = TAILLE_H;
-        SDL_RenderCopy(rend, mur, NULL, &rect_mur);
-        rect_mur.x = x_tour + largeur_tour - largeur_mur;
-        rect_mur.y = 0;
-        rect_mur.w = largeur_mur;
-        rect_mur.h = TAILLE_H;
-        SDL_RenderCopy(rend, mur, NULL, &rect_mur);
-        rect_perso.x = x_tour + 3*largeur_mur;
-        rect_perso.y = TAILLE_H/5;
-        rect_perso.w = largeur_perso;
-        rect_perso.h = hauteur_perso;
-        SDL_RenderCopy(rend, perso, NULL, &rect_perso);
+        aff_fond->render(rend, aff_fond);
+        aff_fond_tour->render(rend, aff_fond_tour);
+        aff_mur_g->render(rend, aff_mur_g);
+        aff_mur_d->render(rend, aff_mur_d);
+        aff_perso->render(rend, aff_perso);
 
         SDL_RenderPresent(rend);
         SDL_Delay(1000/FPS);
     }
+
+    detruire_affichage(&aff_fond);
+    detruire_affichage(&aff_fond_tour);
+    detruire_affichage(&aff_mur_g);
+    detruire_affichage(&aff_mur_d);
+    detruire_affichage(&aff_perso);
 }
