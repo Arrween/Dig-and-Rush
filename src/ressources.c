3252ebc4c340796b774984225f4f39923781e2de
Matthieu Boulanger <matthieu.boulanger.etu@univ-lemans.fr>
2024-03-04 15:00:29 +0100

    ressources.c : gestion des spritesheets (textures avec dimensions des sprites)

--- src/ressources.c
+++ src/ressources.c
@@ -6,6 +6,7 @@
 #include "uthash.h"
 
 t_texture * textures = NULL;
+t_spritesheet * spritesheets = NULL;
 t_son * sons = NULL;
 
 struct chargement {
@@ -13,10 +14,20 @@ struct chargement {
     char id[TAILLE_MAX_ID];
 };
 
+struct chargement_spritesheet {
+    char chemin[TAILLE_MAX_CHEMIN];
+    int sprite_l, sprite_h;
+    char id[TAILLE_MAX_ID];
+};
+
 struct chargement chargements_texs[] = {
-    {"ressources/Personnages/jackPerso.png", "jack"},
     {"ressources/Menu/Background_Menu/Fond_Menu.png", "fond_menu"},
     {"ressources/img/fond_tour.png", "fond_tour"},
+    {"ressources/Personnages/jackPerso.png", "jack"},
+};
+
+struct chargement_spritesheet chargements_spritesheets[] = {
+    {"ressources/Personnages/jackPerso.png", 64, 64, "jack"},
 };
 
 struct chargement chargements_sons[] = {
@@ -25,49 +36,72 @@ struct chargement chargements_sons[] = {
 
 void init_ressources(SDL_Renderer * rend) {
     t_texture * ressource_tex;
+    t_spritesheet * ressource_spritesheet;
     t_son * ressource_son;
-    char chemin[TAILLE_MAX_CHEMIN];
-    char id[TAILLE_MAX_ID];
+    struct chargement charg;
+    struct chargement_spritesheet charg_sheet;
     unsigned long i;
 
     for (i = 0; i < sizeof(chargements_texs)/sizeof(*chargements_texs); i++) {
-        strcpy(chemin, chargements_texs[i].chemin);
-        strcpy(id, chargements_texs[i].id);
-        HASH_FIND_STR(textures, id, ressource_tex);
+        charg = chargements_texs[i];
+        HASH_FIND_STR(textures, charg.id, ressource_tex);
         if (!ressource_tex) {
             ressource_tex = malloc(sizeof(t_texture));
-            ressource_tex->texture = IMG_LoadTexture(rend, chemin);
+            ressource_tex->texture = IMG_LoadTexture(rend, charg.chemin);
             if (!ressource_tex->texture) {
-                fprintf(stderr, "Erreur lors du chargement de la texture "
-                                "« %s » : %s\n", chemin, IMG_GetError());
+                fprintf(stderr, "Erreur lors du chargement d’une texture"
+                                " : %s\n", IMG_GetError());
             }
-            strcpy(ressource_tex->id, id);
+            strcpy(ressource_tex->id, charg.id);
             HASH_ADD_STR(textures, id, ressource_tex);
         }
         else {
             printf("Avertissement : ressource texture avec l’id « %s » "
-                   "déjà présente, fichier « %s » non chargé\n", id, chemin);
+                   "déjà présente, fichier « %s » non chargé\n",
+                   charg.id, charg.chemin);
+        }
+    }
+
+    for (i = 0; i < sizeof(chargements_spritesheets)/sizeof(*chargements_spritesheets); i++) {
+        charg_sheet = chargements_spritesheets[i];
+        HASH_FIND_STR(spritesheets, charg_sheet.id, ressource_spritesheet);
+        if (!ressource_spritesheet) {
+            ressource_spritesheet = malloc(sizeof(t_spritesheet));
+            ressource_spritesheet->texture = IMG_LoadTexture(rend, charg_sheet.chemin);
+            if (!ressource_spritesheet->texture) {
+                fprintf(stderr, "Erreur lors du chargement d’une spritesheet"
+                            " : %s\n", IMG_GetError());
+            }
+            ressource_spritesheet->sprite_l = charg_sheet.sprite_l;
+            ressource_spritesheet->sprite_h = charg_sheet.sprite_h;
+            strcpy(ressource_spritesheet->id, charg_sheet.id);
+            HASH_ADD_STR(spritesheets, id, ressource_spritesheet);
+        }
+        else {
+            printf("Avertissement : ressource spritesheet avec l’id « %s » "
+                   "déjà présente, fichier « %s » non chargé\n",
+                   charg_sheet.id, charg_sheet.chemin);
         }
     }
 
     for (i = 0; i < sizeof(chargements_sons)/sizeof(*chargements_sons); i++) {
-        strcpy(chemin, chargements_sons[i].chemin);
-        strcpy(id, chargements_sons[i].id);
-        HASH_FIND_STR(sons, id, ressource_son);
+        charg = chargements_sons[i];
+        HASH_FIND_STR(sons, charg.id, ressource_son);
         if (!ressource_son) {
             ressource_son = malloc(sizeof(t_son));
-            if (!SDL_LoadWAV(chemin, &(ressource_son->spec),
+            if (!SDL_LoadWAV(charg.chemin, &(ressource_son->spec),
                                 &(ressource_son->buffer),
                                 &(ressource_son->length))) {
-                fprintf(stderr, "Erreur lors du chargement du son "
-                                "« %s » : %s\n", chemin, IMG_GetError());
+                fprintf(stderr, "Erreur lors du chargement d’un son"
+                                " : %s\n", IMG_GetError());
             }
-            strcpy(ressource_son->id, id);
+            strcpy(ressource_son->id, charg.id);
             HASH_ADD_STR(sons, id, ressource_son);
         }
         else {
             printf("Avertissement : ressource son avec l’id « %s » "
-                   "déjà présente, fichier « %s » non chargé\n", id, chemin);
+                   "déjà présente, fichier « %s » non chargé\n",
+                   charg.id, charg.chemin);
         }
     }
 }
@@ -84,6 +118,18 @@ SDL_Texture * recuperer_texture(const char * id) {
     }
 }
 
+t_spritesheet * recuperer_spritesheet(const char * id) {
+    t_spritesheet * ressource;
+    HASH_FIND_STR(spritesheets, id, ressource);
+    if (ressource)
+        return ressource;
+    else {
+        fprintf(stderr, "Avertissement : récupération d’une "
+                "ressource spritesheet inexistante « %s »\n", id);
+        return NULL;
+    }
+}
+
 t_son * recuperer_son(const char * id) {
     t_son * ressource;
     HASH_FIND_STR(sons, id, ressource);
@@ -95,10 +141,15 @@ t_son * recuperer_son(const char * id) {
 
 void detruire_ressources() {
     t_texture * tex_courant, * tex_tmp;
+    t_spritesheet * sheet_courant, * sheet_tmp;
     t_son * son_courant, * son_tmp;
     HASH_ITER(hh, textures, tex_courant, tex_tmp) {
         SDL_DestroyTexture(tex_courant->texture);
         free(tex_courant);
+    }
+    HASH_ITER(hh, spritesheets, sheet_courant, sheet_tmp) {
+        SDL_DestroyTexture(sheet_courant->texture);
+        free(sheet_courant);
     }
     HASH_ITER(hh, sons, son_courant, son_tmp) {
         SDL_FreeWAV(son_courant->buffer);
--- src/ressources.h
+++ src/ressources.h
@@ -12,6 +12,15 @@ typedef struct {
 extern t_texture * textures;
 
 typedef struct {
+    SDL_Texture * texture;
+    int sprite_l, sprite_h;
+    char id[TAILLE_MAX_ID];
+    UT_hash_handle hh;
+} t_spritesheet; 
+
+extern t_spritesheet * spritesheets;
+
+typedef struct {
     SDL_AudioSpec spec;
     uint32_t length;
     uint8_t * buffer;
@@ -24,6 +33,7 @@ extern t_son * sons;
 void init_ressources(SDL_Renderer *);
 
 SDL_Texture * recuperer_texture(const char*);
+t_spritesheet * recuperer_spritesheet(const char*);
 t_son * recuperer_son(const char*);
 
 void detruire_ressources(void);
