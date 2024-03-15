ce365bc0a90406d5e86d8618877158641603f843
Jacques Mpondo <Jacques-Gerard.Mpondo_Toutou.Etu@univ-lemans.fr>
2024-03-15 20:27:55 +0100

    Modification de la constante

--- src/constantes.h
+++ src/constantes.h
@@ -3,11 +3,9 @@
 #define TAILLE_H 720
 #define FPS 60
 
-<<<<<<< HEAD
-#define VITESSE_CHUTE 8. // float, en %age de la hauteur de jeu par frame
-=======
+
 #define VITESSE_CHUTE 3. // float, en %age de la hauteur de jeu par frame
->>>>>>> b1c8312b6ca29469b87268094671a61482796c5a
+
 
 #define COULEUR_HITBOX_R 255
 #define COULEUR_HITBOX_V 255
