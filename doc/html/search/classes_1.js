0eb8b57e81b53d321b4cc5ff52db33c90e6e31a9
Matthieu Boulanger <matthieu.boulanger.etu@univ-lemans.fr>
2024-03-22 10:39:41 +0100

    gitignore : ignorer la documentation générée

--- doc/html/search/classes_1.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['s_5faffichage_0',['s_affichage',['../structs__affichage.html',1,'']]],
-  ['s_5fentite_1',['s_entite',['../structs__entite.html',1,'']]],
-  ['s_5fetat_2',['s_etat',['../structs__etat.html',1,'']]]
-];
