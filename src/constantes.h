3e85fc7e079273c276abbca2a825510a2e99ef1c
Matthieu Boulanger <matthieu.boulanger.etu@univ-lemans.fr>
2024-04-07 02:18:21 +0200

    adaptation des FPS et constantes de vitesses au lourd calcul de l’effet de lumière

--- src/constantes.h
+++ src/constantes.h
@@ -1,10 +1,10 @@
 #define TITRE_FENETRE "Dig & Rush"
 #define TAILLE_L 1280
 #define TAILLE_H 720
-#define FPS 60
+#define FPS 30
 
 
-#define VITESSE_CHUTE 1/2. // float, en %age de la hauteur de jeu par frame
+#define VITESSE_CHUTE 2./1 // float, en %age de la hauteur de jeu par frame
 
 
 #define COULEUR_HITBOX_R 255
