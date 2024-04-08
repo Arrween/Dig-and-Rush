b12575641501e68cf33230780acaae197d793ecd
Matthieu Boulanger <matthieu.boulanger.etu@univ-lemans.fr>
2024-04-08 18:23:36 +0200

    comportement_patrouille : se retourner quand rencontre un obstacle

--- src/entite_pnj.c
+++ src/entite_pnj.c
@@ -11,11 +11,16 @@ void comportement_oisif(t_entite * pnj) {
 void comportement_patrouille(t_entite * pnj) {
     if (pnj->pnj->est_mort)
         return;
-    if (pnj->deplacement == REPOS_MVT || pnj->deplacement == DROITE && pnj->hitbox.x + pnj->hitbox.w >= pnj->pnj->x_patrouille_d) {
+    if (pnj->deplacement == REPOS_MVT
+        || pnj->deplacement == DROITE && pnj->hitbox.x + pnj->hitbox.w >= pnj->pnj->x_patrouille_d
+        || pnj->a_collision_d
+        ) {
         pnj->deplacement = GAUCHE;
         changer_animation(pnj, DEPL_G);
     }
-    else if (pnj->deplacement == GAUCHE && pnj->hitbox.x <= pnj->pnj->x_patrouille_g) {
+    else if (pnj->deplacement == GAUCHE && pnj->hitbox.x <= pnj->pnj->x_patrouille_g
+             || pnj->a_collision_g
+             ) {
         pnj->deplacement = DROITE;
         changer_animation(pnj, DEPL_D);
     }
