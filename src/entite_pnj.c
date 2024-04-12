b421a72ae4549ba3ec4df8ea868b06d000ce946a
Matthieu Boulanger <matthieu.boulanger.etu@univ-lemans.fr>
2024-04-13 00:05:00 +0200

    ajout de hitboxes d’attaque pour les pnj

--- src/entite_pnj.c
+++ src/entite_pnj.c
@@ -4,6 +4,9 @@
 #include "entite.h"
 #include "constantes.h"
 
+
+void changer_hitbox_attaque(t_pnj*, float, float, float, float, int);
+
 /**
  * @brief Comportement par défaut pour un PNJ oisif.
  */
@@ -21,30 +24,60 @@ void comportement_oisif(void) {
 void comportement_patrouille(t_entite * pnj) {
     if (pnj->pnj->est_mort)
         return;
+    int a_change_sens = FAUX;
     if (pnj->deplacement == REPOS_MVT
         || (pnj->deplacement == DROITE && pnj->hitbox.x + pnj->hitbox.w >= pnj->pnj->x_patrouille_d)
         || pnj->collisions.d
         ) {
         pnj->deplacement = GAUCHE;
         changer_animation(pnj, DEPL_G);
+        changer_pos_rel(pnj, -1, 0); // pour sortir de l’état de collision
+        if (pnj->deplacement != REPOS_MVT)
+            a_change_sens = VRAI;
     }
     else if ((pnj->deplacement == GAUCHE && pnj->hitbox.x <= pnj->pnj->x_patrouille_g)
              || pnj->collisions.g
              ) {
         pnj->deplacement = DROITE;
         changer_animation(pnj, DEPL_D);
+        changer_pos_rel(pnj, 1, 0); // pour sortir de l’état de collision
+        a_change_sens = VRAI;
     }
     if (!pnj->collisions.b) {
         if (pnj->deplacement == GAUCHE) {
             pnj->deplacement = DROITE;
             changer_animation(pnj, DEPL_D);
-            changer_pos_rel(pnj, 1, 0);
+            changer_pos_rel(pnj, 1, 0); // pour sortir de l’état de non-collision
+            a_change_sens = VRAI;
         }
         else if (pnj->deplacement == DROITE) {
             pnj->deplacement = GAUCHE;
             changer_animation(pnj, DEPL_G);
-            changer_pos_rel(pnj, -1, 0);
+            changer_pos_rel(pnj, -1, 0); // pour sortir de l’état de non-collision
+            a_change_sens = VRAI;
         }
+    }
+    if (a_change_sens)
+        changer_hitbox_attaque(pnj->pnj,
+           2*pnj->hitbox.x + pnj->hitbox.w - pnj->pnj->hitbox_attaque.x - pnj->pnj->hitbox_attaque.w,
+           pnj->pnj->hitbox_attaque.y,
+           pnj->pnj->hitbox_attaque.w,
+           pnj->pnj->hitbox_attaque.h,
+           FAUX);
+}
+
+void changer_hitbox_attaque(t_pnj * pnj, float x, float y, float w, float h, int avec_coord_rect_dst) {
+    if (avec_coord_rect_dst) {
+        pnj->hitbox_attaque.x = pnj->parent->rect_dst->x + pnj->parent->rect_dst->w * (float)x/100;
+        pnj->hitbox_attaque.y = pnj->parent->rect_dst->y + pnj->parent->rect_dst->h * (float)y/100;
+        pnj->hitbox_attaque.w = pnj->parent->rect_dst->w * (float)w/100;
+        pnj->hitbox_attaque.h = pnj->parent->rect_dst->h * (float)h/100;
+    }
+    else {
+        pnj->hitbox_attaque.x = x;
+        pnj->hitbox_attaque.y = y;
+        pnj->hitbox_attaque.w = w;
+        pnj->hitbox_attaque.h = h;
     }
 }
 
@@ -58,15 +91,17 @@ void comportement_patrouille(t_entite * pnj) {
 
 t_pnj * creer_pnj(char * id, t_entite * e) {
     t_pnj * nouv = malloc(sizeof(t_pnj));
+    nouv->parent = e;
     if (strcmp(id, "squelette") == 0) {
         nouv->comportement = comportement_patrouille;
         nouv->valeur_vaincu = 10;
         nouv->est_ecrasable = VRAI;
-        e->vitesse = 1./2;
-        changer_hitbox(e, 20, 50, 50, 55);
-    }
-    else if (strcmp(id, "feu") == 0) {
-        // champs initiaux pour la boule de feu
+        nouv->parent->vitesse = 1./2;
+        changer_hitbox(nouv->parent, 30, 50, 40, 55);
+        // définir initialement sur la droite, sera modifié par la patrouille
+        changer_hitbox_attaque(nouv, 70, 70, 20, 20, VRAI);
+        nouv->doit_afficher_hitbox_attaque = VRAI;
+        nouv->parent->doit_afficher_hitbox = VRAI;
     }
     else {
         nouv->comportement = (void(*)(t_entite*)) comportement_oisif;
