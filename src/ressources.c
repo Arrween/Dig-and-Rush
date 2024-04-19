6cdceed5251a9a3d82c38821d9e73abe4a0d05f2
Ania Garoui <ania.garoui.etu@univ-lemans.fr>
2024-04-19 18:11:20 +0200

    Ajout d'accroche

--- src/ressources.c
+++ src/ressources.c
@@ -231,6 +231,7 @@ struct chargement chargements_sons[] = {
     {"ressources/Audio/SFX/menu_erreur.ogg", "erreur"},
     {"ressources/Audio/SFX/accroche_matthieu.ogg", "accroche_matt"},
     {"ressources/Audio/SFX/accroche_jacques.ogg", "accroche_jack"},
+    {"ressources/Audio/SFX/accroche_ania.ogg", "accroche_ania"},
 
 
     {"ressources/Audio/SFX/coq.mp3", "coq"},
